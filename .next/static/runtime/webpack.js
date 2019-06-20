15094517221963302275059953c264c05a9010d1
wkwk2805 <wkwk2805@gmail.com>
2019-06-20 09:26:41 +0900

    file test

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2a868f0df06509e8ea74";
+/******/ 	var hotCurrentHash = "5d022c821cbad5d04ef0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 // list시 카드를 활용해서 하면 좋을듯
 const Home = () => {
-  return <div>Hello Home</div>;
+  return <div>Hello Homes</div>;
 };
 
 export default Home;
